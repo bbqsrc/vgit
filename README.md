a1eebd3b5f321ec4226d6d1d4d851bcddf6e0a47
Brendan Molloy <brendan@bbqsrc.net>
2016-02-07 16:28:22 +1100

    Early prototype

---
+++ README.md
@@ -0,0 +1,7 @@
+# vgit
+
+A web viewer for git repositories.
+
+## License
+
+BSD 2-clause. See LICENSE.
---
+++ index.js
@@ -0,0 +1,187 @@
+/*!
+ * Copyright (c)  2016 Brendan Molloy <brendan@bbqsrc.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+'use strict';
+
+const Git = require('nodegit'),
+      Promise = require('bluebird'),
+      fs = Promise.promisifyAll(require('fs')),
+      koa = require('koa'),
+      path = require('path'),
+      Router = require('koa-rutt'),
+      hbs = require('koa-hbs'),
+      config = require('./config'),
+      app = koa(),
+      router = new Router();
+
+const PATH = config.path;
+
+app.use(hbs.middleware({
+  defaultLayout: 'default',
+  viewPath: __dirname + '/templates'
+}));
+
+function genPath(ctx, root, type) {
+  const ref = root.ref.shorthand();
+  const filename = path.basename(ctx.path());
+  const basePath = root.basePath || '';
+
+  return path.join(`/${root.repoName}/${type}/${ref}`, basePath, filename);
+}
+
+hbs.registerHelper('blob', function(options) {
+  return genPath(this, options.data.root, 'blob');
+});
+
+hbs.registerHelper('tree', function(options) {
+  return genPath(this, options.data.root, 'tree');
+});
+
+function getBranches(refs) {
+  return refs.filter(r => r.isBranch()).map(r => r.shorthand());
+}
+
+function* parseReferenceFromPath(repo, param) {
+  const refs = yield repo.getReferences(Git.Reference.TYPE.LISTALL);
+
+  for (const ref of refs) {
+    const sh = ref.shorthand();
+    if (param.startsWith(sh)) {
+      return { ref, path: param.substring(sh.length + 1) };
+    }
+  }
+
+  return null;
+}
+
+router
+.get('/', function* getIndex() {
+  const files = yield fs.readdirAsync(PATH);
+  const flags = Git.Repository.OPEN_FLAG.OPEN_NO_SEARCH |
+                Git.Repository.OPEN_FLAG.OPEN_CROSS_FS |
+                Git.Repository.OPEN_FLAG.OPEN_BARE;
+
+  const repos = [];
+
+  for (const fn of files) {
+    const fpath = path.join(PATH, fn);
+
+    try {
+      const repo = yield Git.Repository.openExt(fpath + '/.git', flags, PATH);
+      if (repo) {
+        repo.name = fn;
+        repos.push(repo);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  yield this.render('index', { repos });
+})
+.get('/:repo', function* getRepo() {
+  const repoPath = path.join(PATH, this.params.repo);
+  const repo = yield Git.Repository.open(repoPath);
+
+  const commit = yield repo.getMasterCommit();
+  const ref = yield repo.getReference('heads/master');
+  const tree = yield commit.getTree();
+
+  const directories = tree.entries().filter(entry => entry.isDirectory());
+  const files = tree.entries().filter(entry => !entry.isDirectory());
+
+  yield this.render('tree', {
+    repoName: this.params.repo,
+    ref: ref,
+    tree, files, directories
+  });
+})
+.get('/:repo/tree/:path(.*)', function* getRepoTree() {
+  const repoPath = path.join(PATH, this.params.repo);
+  const repo = yield Git.Repository.open(repoPath);
+
+  const res = yield parseReferenceFromPath(repo, this.params.path);
+  const commit = yield repo.getReferenceCommit(res.ref);
+  let tree = yield commit.getTree();
+
+  if (res.path !== "") {
+    const entry = yield tree.entryByPath(res.path);
+    // &wtf;
+    entry.parent = tree;
+    tree = yield entry.getTree();
+  }
+
+  const directories = tree.entries().filter(entry => entry.isDirectory());
+  const files = tree.entries().filter(entry => !entry.isDirectory());
+
+  yield this.render('tree', {
+    repoName: this.params.repo,
+    ref: res.ref,
+    basePath: res.path,
+    tree, files, directories
+  });
+})
+.get('/:repo/blob/:path(.*)', function* getRepoBlob() {
+  const repoPath = path.join(PATH, this.params.repo);
+  const repo = yield Git.Repository.open(repoPath);
+
+  const res = yield parseReferenceFromPath(repo, this.params.path);
+  const commit = yield repo.getReferenceCommit(res.ref);
+  const tree = yield commit.getTree();
+
+  console.log(res.path);
+  const blobEntry = yield tree.entryByPath(res.path);
+  blobEntry.parent = tree;
+
+  if (blobEntry.isBlob()) {
+    const blob = yield blobEntry.getBlob();
+    const content = blob.isBinary() ? "(Binary not shown)" : blob.toString();
+
+    yield this.render('blob', {
+      fcontent: content,
+      fsize: blob.rawsize(),
+      filename: path.basename(res.path)
+    });
+    return;
+  }
+})
+.get('/:repo/raw/:path(.*)', function* getRepoRaw() {
+
+})
+.get('/:repo/commit/:hash', function* getRepoCommit() {
+
+})
+.get('/:repo/branches', function* getRepoBranches() {
+  const repoPath = path.join(PATH, this.params.repo);
+  const repo = yield Git.Repository.open(repoPath);
+
+  const refs = yield repo.getReferences(Git.Reference.TYPE.LISTALL);
+  return (this.body = getBranches(refs));
+});
+
+app.use(router.middleware());
+
+app.listen(3001);
